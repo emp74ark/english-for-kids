dab0b104527d1be27c1e843b60d08f2ac2b071f9
emp74ark <andrey.yurkovsky@gmail.com>
2022-09-07 20:37:36 +0200

    feat: second language for cards

--- src/js/main.js
+++ src/js/main.js
@@ -11,18 +11,76 @@ async function genChapters(){
     const card__img = document.createElement('img');
     card__img.src = chapter.img;
     const card__name = document.createElement('span');
+    card__name.className = 'card__name';
     card__name.textContent = chapter.chapter;
+    const card__counter = document.createElement('span');
+    card__counter.className = 'card__counter';
+    card__counter.textContent = chapter.words.length
     card.appendChild(card__img);
     card.appendChild(card__name);
+    card.appendChild(card__counter);
+    card.addEventListener('click', () => {
+      genCards(chapter.chapter)
+    })
     fragment.appendChild(card);
   }
   return fragment;
 }
 
 async function applyChapters(){
+  main.innerHTML = '';
   const chapters = await genChapters()
   main.appendChild(chapters)
   document.body.appendChild(main)
 }
 
-export { applyChapters }
+async function genCards(chapter) {
+  const fragment = new DocumentFragment()
+  const words = await getWords();
+  for (const c of words){
+    if(c.chapter === chapter){
+      for (const word of c.words){
+        const card = document.createElement('div');
+        card.className = 'card';
+        const card_en = document.createElement('div');
+        card_en.className = 'card_en';
+        const card__img_en = document.createElement('img');
+        card__img_en.src = word.img;
+        const card__name_en = document.createElement('span');
+        card__name_en.className = 'card__name';
+        card__name_en.textContent = word.en;
+        card_en.appendChild(card__img_en);
+        card_en.appendChild(card__name_en);
+        card.appendChild(card_en)
+        const card_ru = document.createElement('div');
+        card_ru.classList.add('card_ru');
+        card_ru.classList.add('card_hidden')
+        const card__img_ru = document.createElement('img');
+        card__img_ru.src = word.img;
+        const card__name_ru = document.createElement('span');
+        card__name_ru.className = 'card__name';
+        card__name_ru.textContent = word.ru;
+        card_ru.appendChild(card__img_ru);
+        card_ru.appendChild(card__name_ru);
+        card.appendChild(card_en)
+        card.appendChild(card_ru)
+        
+        const button_rotate = document.createElement('img');
+        button_rotate.src = '../assets/icons/rotate.png';
+        button_rotate.className = 'button__rotate';
+        card.appendChild(button_rotate);
+        button_rotate.addEventListener('click', () => {
+          card_en.classList.toggle('card_hidden');
+          card_ru.classList.toggle('card_hidden');
+        })
+        
+
+        fragment.appendChild(card);
+      }
+    }
+  }
+  main.innerHTML = '';
+  main.appendChild(fragment);
+}
+
+export { applyChapters, genCards }
--- src/js/nav.js
+++ src/js/nav.js
@@ -1,4 +1,5 @@
 import { getWords } from './sources';
+import { applyChapters, genCards } from './main';
 
 const nav = document.createElement('nav');
 
@@ -10,21 +11,43 @@ async function genNav(){
   const burger__lines = document.createElement('span');
   burger__lines.className = 'burger__lines';
   burger__wrapper.appendChild(burger__lines);
-  burger__wrapper.addEventListener('click', () => {
+  function burgerToggle(){
     burger__wrapper.classList.toggle('burger__wrapper_active');
     nav__list.classList.toggle('nav__list_active')
+  }
+  burger__wrapper.addEventListener('click', () => {
+    burgerToggle();
   })
   
   const words = await getWords();
   const nav__list = document.createElement('ul');
+  const nav__list_main = document.createElement('li');
+  nav__list_main.textContent = 'Main page'
+  nav__list.appendChild(nav__list_main);
   nav__list.className = 'nav__list'
   for (const chapter of words){
     const nav__item = document.createElement('li');
     nav__item.textContent = chapter.chapter;
     nav__list.appendChild(nav__item);
   }
+  const nav__list_score = document.createElement('li');
+  nav__list_score.textContent = 'Score'
+  nav__list.appendChild(nav__list_score);
   nav__list.addEventListener('click', (e) => {
-    console.log(e.target)
+    const tag = e.target.closest('li').textContent;
+    switch (tag) {
+      case 'Main page':
+        applyChapters();
+        burgerToggle();
+        break;
+      case 'Score':
+        burgerToggle();
+        console.log('score')
+        break;
+      default:
+        genCards(tag);
+        burgerToggle();
+    }
   })
   
   fragment.appendChild(burger__wrapper);
